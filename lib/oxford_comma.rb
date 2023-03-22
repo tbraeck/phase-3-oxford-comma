598b110aebf8d557cafa7966bf224784a69d2536
Tate <tatebraeckel@gmail.com>
2023-03-21 18:36:49 -0600

    commit tree cca34e039d06ca9c9883e0d2ed0d86ac8339a1a1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
+# takes array of str elements as an argument and converrts it to string using oxford comma 
 
+    return array.join(" and ") if array.size < 3
+
+    array[-1] = "and #{array[-1]}"
+
+    array.join(", ")
 end
